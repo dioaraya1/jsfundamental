f5f58fbccac4ee7bf269ab016813e2c8a70d90c4
dioaraya1 <dioaraya1@gmail.com>
2025-12-24 19:06:55 +0700

    commit

---
+++ javascript-course-beginner/10-Functions/script.js
@@ -0,0 +1,10 @@
+'use strict';
+const bookings = [];
+
+const createBooking = function (
+  hotelName,
+  { roomType = 'standard', nights = 1, guest = 1, price }
+) {
+  //validasi input
+  if (!hotel)
+};
