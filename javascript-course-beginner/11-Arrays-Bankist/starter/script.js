ba07e0a50c30e034f1002cb70c477cd77922bcbf
dioaraya1 <dioaraya1@gmail.com>
2026-01-15 16:57:34 +0700

    commit

--- javascript-course-beginner/11-Arrays-Bankist/starter/script.js
+++ javascript-course-beginner/11-Arrays-Bankist/starter/script.js
@@ -63,16 +63,8 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
+
 // LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 /////////////////////////////////////////////////
 
 // Simple Array Methods
@@ -86,6 +78,16 @@ console.log(arr.slice(-1));
 console.log(arr.slice(1, -2));
 console.log(arr.slice());
 console.log([...arr]);
+
+const buah = ['jeruk', 'apel', 'mangga', 'pisang', 'pepaya'];
+// slice tanpa mengubah array asli
+const favorite = buah.slice(1, 3);
+console.log(favorite);
+console.log(buah);
+// splice mengubah array asli
+const hapusBuah = buah.splice(1, 3);
+console.log(hapusBuah);
+console.log(buah);
 
 // SPLICE
 // console.log(arr.splice(2));
@@ -107,3 +109,69 @@ console.log([...arr, ...arr2]);
 
 // JOIN
 console.log(letters.join(' - '));
+
+///////////////////////////////////////
+// Looping Arrays: forEach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---- FOREACH ----');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
+
+///////////////////////////////////////
+// forEach With Maps and Sets
+// Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
+
+const displayMovements = function (movements) {
+  movements.forEach(function (mov, i) {
+    containerMovements.innerHTML = '';
+    // .textContent = 0
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
