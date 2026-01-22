8d99a491d90a3450bd4a8e58c795ea4b9461789a
dioaraya1 <dioaraya1@gmail.com>
2026-01-22 15:08:50 +0700

    find and findlast method

--- javascript-course-beginner/11-Arrays-Bankist/starter/script.js
+++ javascript-course-beginner/11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,10 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+//! username & password
+inputLoginUsername.value = 'js';
+inputLoginPin.value = '1111';
+
 /////////////////////////////////////////////////
 // Functions
 const displayMovements = function (movements) {
@@ -212,7 +216,6 @@ btnClose.addEventListener('click', function (e) {
     inputCloseUsername.value = inputClosePin.value = '';
   }
 });
-
 
 /////////////////////////////////////////////////
 // LECTURES
@@ -490,8 +493,10 @@ const totalDepositsUSD = movements
 console.log(totalDepositsUSD);
 */
 
+// /*
+///////////////////////////////////////
 // The find Method
-/*
+
 const firstWithdrawal = movements.find(mov => mov < 0);
 console.log(movements);
 console.log(firstWithdrawal);
@@ -499,4 +504,22 @@ console.log(firstWithdrawal);
 console.log(accounts);
 const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 console.log(account);
-*/
+
+///////////////////////////////////////
+// The New findLast and findLastIndex Methods
+
+console.log(movements);
+const lastWithdrawal = movements.findLast(mov => mov < 0);
+console.log(lastWithdrawal);
+
+// 'Your latest large movement was X movements ago'
+
+const latestLargeMovementIndex = movements.findLastIndex(
+  mov => Math.abs(mov) > 1000,
+);
+console.log(latestLargeMovementIndex);
+console.log(
+  `Your latest large movement was ${
+    movements.length - latestLargeMovementIndex
+  } movements ago`,
+);
