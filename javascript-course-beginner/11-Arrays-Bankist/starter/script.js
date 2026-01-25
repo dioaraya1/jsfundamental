2c051745d2bf4f4a4434eae06498811754537a97
dioaraya1 <dioaraya1@gmail.com>
2026-01-25 10:11:06 +0700

    btn loan

--- javascript-course-beginner/11-Arrays-Bankist/starter/script.js
+++ javascript-course-beginner/11-Arrays-Bankist/starter/script.js
@@ -537,5 +537,25 @@ console.log(
   } movements ago`,
 );
 
-const latestLargeMovement = movements.findLast(mov => Math.abs(mov) > 1000);
-console.log(latestLargeMovement);
+///////////////////////////////////////
+// some and every
+console.log(movements);
+
+// EQUALITY
+console.log(movements.includes(-130));
+
+// SOME: CONDITION
+console.log(movements.some(mov => mov === -130));
+
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+// EVERY
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
