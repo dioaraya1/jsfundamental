6b2c9fcb2d5ba782ca17bc7fa81270f5fb00acea
dioaraya1 <dioaraya1@gmail.com>
2026-01-29 12:59:34 +0700

    commit

--- javascript-course-beginner/11-Arrays-Bankist/starter/script.js
+++ javascript-course-beginner/11-Arrays-Bankist/starter/script.js
@@ -809,13 +809,3 @@ labelBalance.addEventListener('click', function () {
   );
   console.log(movementsUI);
 });
-
-const calcAverageHumanAge = function (ages) {
-  const humanAges = ages
-    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
-    .filter(age => age >= 18)
-    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
-  console.log(humanAges);
-};
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
